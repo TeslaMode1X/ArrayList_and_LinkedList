6d7ece464cf0f594cff329f3ae5fb73cfffd7498
Tesla <anuar.musljumov@gmail.com>
2024-04-12 15:05:02 +0500

    Created my own ArrayList as MyArrayList

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        MyArrayList list = new MyArrayList();
+
+
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.sort();
+
+        printArray(list);
+
+        System.out.println(list + " " + list.size() + " " + list.length());
+    }
+
+    public static <T> void printArray(MyArrayList list) {
+        for (Object item : list) {
+            System.out.println(item);
+        }
     }
 }
